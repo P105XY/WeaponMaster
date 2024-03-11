e86c0f093b135b1b4d13d0118d6457ac7663678c
P105XY <p105xy85@gmail.com>
2024-03-11 22:40:02 +0900

    프로젝트 재설정

---
+++ Assets/Source/CharacterClass/CharacterDataStruct.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterDataNamespace
+{
+    public struct CharacterDataStruct
+    {
+        public string characterName;
+        public int characterLevel;
+        public int characterHealth;
+        public int characterMana;
+        public int characterStrength;
+        public int characterAgility;
+        public int characterAccuracy;
+        public int characterTechnique;
+    }
+}
